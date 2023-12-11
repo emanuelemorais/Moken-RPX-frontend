efb90838fa8c1b007506f4bb0bf54a3d9d6f7c03
Elisa Flemer <elisaflemer@gmail.com>
2023-12-10 23:54:49 -0300

    working

--- app/addProperty/page.js
+++ app/addProperty/page.js
@@ -8,47 +8,99 @@ import { ethers } from "ethers";
 import axios from "axios";
 import ImageUploader from "@/components/ImageUploader";
 import dotenv from "dotenv";
+import { useMetaMask } from "@/contexts/WalletContext";
 dotenv.config();
 
-
 export default function Rentals() {
+  const [displayImage, setDisplayImage] = useState(null);
   const [imageData, setImageData] = useState(null);
-  const [cid, ]
+  const [cid, setCid] = useState("");
+  const [finalCid, setFinalCid] = useState("");
+  const { account, connectMetaMask } = useMetaMask();
+
+  const [propertyData, setPropertyData] = useState({
+    type: "House", // Default to 'House'
+    address: "",
+    bedrooms: 0,
+    size: 0,
+  });
+  const [propertyContract, setPropertyContract] = useState(null);
+
+  useEffect(() => {
+    const InitializePropertiesContract = async () => {
+      const contractAddress = "0x330D0349ed3c5A8a212CC15EeBA92A6b4807dDF4";
+      const contractABI = Moken.abi;
+      let provider = new ethers.BrowserProvider(window.ethereum);
+      let signer = await provider.getSigner();
+      const contract = new ethers.Contract(
+        contractAddress,
+        contractABI,
+        signer
+      );
+
+      console.log("contract", contract);
+      setPropertyContract(contract);
+
+      return contract;
+    };
+
+    InitializePropertiesContract();
+  }, []);
 
   const uploadFile = async (imageData) => {
     try {
       const formData = new FormData();
-  
+
       formData.append("file", imageData);
-  
-      const res = await fetch("https://api.pinata.cloud/pinning/pinFileToIPFS", {
-        method: "POST",
-        headers: {
-          Authorization: `Bearer ${process.env.NEXT_PUBLIC_PINATA_JWT}`,
-        },
-        body: formData,
-      });
+
+      const res = await fetch(
+        "https://api.pinata.cloud/pinning/pinFileToIPFS",
+        {
+          method: "POST",
+          headers: {
+            Authorization: `Bearer ${process.env.NEXT_PUBLIC_PINATA_JWT}`,
+          },
+          body: formData,
+        }
+      );
       const json = await res.json();
       console.log(json);
       const { IpfsHash } = json;
+      setCid(IpfsHash);
     } catch (e) {
       console.log(e);
       alert("Trouble uploading file");
     }
   };
-  
-  const pinJson = () => {
-  
-    const json = {
-      "type": "House",
-      "address": "456 Oak Avenue",
-      "bedrooms": 4,
-      "size": 250,
-      "image": "https://ipfs.io/ipfs/QmfZpAZzMNLYEq5ApRcNH1czrK6xYeeBPg7qEtTd4mtF1X/1.jpg"
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    if (name == "bedrooms" || name == "size") {
+      value = parseInt(value);
     }
+
+    setPropertyData((prevData) => ({ ...prevData, [name]: value }));
+  };
+
+  const pinJson = async () => {
+    let data = propertyData;
+    data.image = `https://ipfs.io/ipfs/${cid}`;
+
+    const res = await fetch("https://api.pinata.cloud/pinning/pinJSONToIPFS", {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${process.env.NEXT_PUBLIC_PINATA_JWT}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+    const json = await res.json();
+    setFinalCid(json.IpfsHash);
+    console.log(json);
   };
 
   const handleImageUpload = (dataUrl) => {
+    setDisplayImage(dataUrl);
     // Convert data URL to Blob
     const base64String = dataUrl.split(",")[1];
     const mimeType = dataUrl.split(";")[0].slice(5);
@@ -74,7 +126,9 @@ export default function Rentals() {
   };
 
   const save = async () => {
-    uploadFile(imageData);
+    await uploadFile(imageData);
+    await pinJson();
+    await propertyContract.newProperty("name", "symbol", `https://ipfs.io/ipfs/${finalCid}`, 500, account);
   };
   return (
     <div className="">
@@ -86,9 +140,65 @@ export default function Rentals() {
               Adicionar propriedade
             </h1>
           </span>
+          {/* Input fields for property details */}
+          <div className="my-4">
+            <label htmlFor="type" className="mr-2">
+              Type:
+            </label>
+            <select
+              id="type"
+              name="type"
+              value={propertyData.type}
+              onChange={handleInputChange}
+            >
+              <option value="House">House</option>
+              <option value="Apartment">Apartment</option>
+              <option value="Hostel">Hostel</option>
+            </select>
+          </div>
+          <div className="my-4">
+            <label htmlFor="address" className="mr-2">
+              Address:
+            </label>
+            <input
+              type="text"
+              id="address"
+              name="address"
+              value={propertyData.address}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div className="my-4">
+            <label htmlFor="bedrooms" className="mr-2">
+              Bedrooms:
+            </label>
+            <input
+              type="number"
+              id="bedrooms"
+              name="bedrooms"
+              value={propertyData.bedrooms}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div className="my-4">
+            <label htmlFor="size" className="mr-2">
+              Size:
+            </label>
+            <input
+              type="number"
+              id="size"
+              name="size"
+              value={propertyData.size}
+              onChange={handleInputChange}
+            />
+          </div>
           <ImageUploader onImageUpload={handleImageUpload} />
-          {imageData && (
-            <img src={imageData} alt="Uploaded" className="mt-4 max-w-full" />
+          {displayImage && (
+            <img
+              src={displayImage}
+              alt="Uploaded"
+              className="mt-4 max-w-full"
+            />
           )}
         </div>
         <button onClick={save}>Adicionar</button>
--- app/rentals/page.js
+++ app/rentals/page.js
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import Navbar from "@/components/Navbar";
 import SearchField from "@/components/SearchField";
@@ -11,24 +11,45 @@ import { ethers } from "ethers";
 export default function Rentals() {
   const [rentals, setRentals] = useState([]); // useState for storing all rental data
   const [filteredRentals, setFilteredRentals] = useState([]); // useState for storing filtered rental data
-  const [searchTerm, setSearchTerm] = useState(''); // useState for storing search term
+  const [searchTerm, setSearchTerm] = useState(""); // useState for storing search term
   const [propertyContract, setPropertyContract] = useState(null);
   const [propertyAddress, setPropertyAddress] = useState(null);
   const [updated, setUpdated] = useState(false);
 
+  var provider = null;
+  var signer = null;
 
-
-  var provider = null
-  var signer = null
-
-  useEffect(() => {
-    const fetchURIJson = async (uri) => {
+  const fetchURIJson = async (uri) => {
+    try {
+      console.log("fetching json for uri", uri);
+      if (
+        uri ===
+          `https://ipfs.io/ipfs/"https://ipfs.io/ipfs/QmNhU3rSkUePaABwqUWxgcowK1G52pPnxNzU7nGyYkEgac` ||
+        uri === "https://ipfs.io/ipfs/" ||
+        uri ===
+          "https://ipfs.io/ipfs/QmcfQx3Re3thVLHLfQC3umM6jBfQbpohmGkPmFDDUXfkUh"
+      ) {
+        console.warn(`Skipping rental for URI ${uri} due to empty URI`);
+        return null;
+      }
       const response = await fetch(uri);
+
+      if (response.status === 422) {
+        console.warn(
+          `Skipping rental for URI ${uri} due to 422 Unprocessable Entity`
+        );
+        return null;
+      }
+
       const json = await response.json();
       return json;
+    } catch (error) {
+      console.error(`Error fetching JSON for URI ${uri}:`, error);
+      return null;
     }
+  };
 
-
+  useEffect(() => {
     const InitializePropertiesContract = async () => {
       const contractAddress = "0x330D0349ed3c5A8a212CC15EeBA92A6b4807dDF4";
       const contractABI = Moken.abi;
@@ -40,7 +61,6 @@ export default function Rentals() {
         signer
       );
 
-      console.log("contract", contract);
       setPropertyContract(contract);
 
       return contract;
@@ -48,41 +68,47 @@ export default function Rentals() {
 
     const fetchRental = async () => {
       try {
-        console.log("fetching rentals");
-
         const property_contract = await InitializePropertiesContract();
-        console.log("initialized properties contract");
 
-        const properties = await property_contract.getAllProperties();
-        console.log("got all properties");
+        let properties = await property_contract.getAllProperties();
+
+        let itemsToRemove = ['0x3c7828F277000DFFa2876b997Edb4c2f92E2c40b', '0xb8eBf3aa6c7fbbbbE7ED74464BFe7C03fa62895E', '0xD808867510D8B5EE4a4EB52a3F5e8B94f1b71Be6', '0x6985bF2D27060D9eb635529935d32C2Db5b5E024'];
+
+        properties = properties.filter(
+          (item) => !itemsToRemove.includes(item)
+        );
 
         let id = 0;
-        const rentalData = await Promise.all(properties.map(async (address) => {
-          console.log("fetching property data for address", address);
-          const contract = new ethers.Contract(address, Property.abi, signer);
-          console.log("initialized property contract");
-
-          const propertyData = await contract.property();
-          const struct_property_data = {...propertyData};
-          console.log("got property data", struct_property_data);
-          const uri = struct_property_data[0];
-          const json = await fetchURIJson(uri);
-          return {
-            address: json["address"],
-            contract: address,
-            id: id++,
-            type: json["type"],
-            price: struct_property_data[2].toString(),
-            bedrooms: json["bedrooms"],
-            size: json["size"],
-            image: json["image"],
-          }
-        }));
+        const rentalData = await Promise.all(
+          properties.map(async (address) => {
+            console.log("fetching property data for address", address);
+            const contract = new ethers.Contract(address, Property.abi, signer);
+            const propertyData = await contract.property();
+            const struct_property_data = { ...propertyData };
+            console.log("got property data", struct_property_data);
+            const uri = struct_property_data[0];
+            const json = await fetchURIJson(uri);
+            if (json === null) {
+              // Skip this rental
+              return null;
+            }
+            return {
+              address: json["address"],
+              contract: address,
+              id: id++,
+              type: json["type"],
+              price: struct_property_data[2].toString(),
+              bedrooms: json["bedrooms"],
+              size: json["size"],
+              image: json["image"],
+            };
+          })
+        );
         setRentals(rentalData);
         console.log(rentalData);
         setFilteredRentals(rentalData);
       } catch (error) {
-        console.error('Error fetching rentals:', error);
+        console.error("Error fetching rentals:", error);
       }
     };
 
@@ -93,42 +119,42 @@ export default function Rentals() {
     }
   }, [propertyContract, rentals, updated]);
 
-   const onSearch = (searchTerm) => {
-     setSearchTerm(searchTerm);
-     filterRentals(searchTerm);
-   };
+  const onSearch = (searchTerm) => {
+    setSearchTerm(searchTerm);
+    filterRentals(searchTerm);
+  };
 
-   const filterRentals = (searchTerm) => {
-     const filtered = rentals.filter((blockchainRental) => {
-       const rentalValues = Object.values(blockchainRental).map((value) =>
-         typeof value === 'string' ? value.toLowerCase() : value
-       );
+  const filterRentals = (searchTerm) => {
+    const filtered = rentals.filter((blockchainRental) => {
+      const rentalValues = Object.values(blockchainRental).map((value) =>
+        typeof value === "string" ? value.toLowerCase() : value
+      );
 
-       return rentalValues.some((value) =>
-         String(value).includes(searchTerm.toLowerCase())
-       );
-     });
+      return rentalValues.some((value) =>
+        String(value).includes(searchTerm.toLowerCase())
+      );
+    });
 
-     setFilteredRentals(filtered);
-   };
+    setFilteredRentals(filtered);
+  };
 
-   const resetFilter = () => {
-     setFilteredRentals(rentals);
-     setSearchTerm('');
-   };
+  const resetFilter = () => {
+    setFilteredRentals(rentals);
+    setSearchTerm("");
+  };
 
   return (
     <div className="">
       <Navbar />
       <div className="flex flex-col justify-center items-center mx-6 md:mx-36 my-8 md:my-16">
-      <div className="px-6 ">
-        <span className="flex justify-between items-center">
-          <h1 className="mx-8 my-4 font-bold text-3xl pb-8">All rentals</h1>
+        <div className="px-6 ">
+          <span className="flex justify-between items-center">
+            <h1 className="mx-8 my-4 font-bold text-3xl pb-8">All rentals</h1>
 
-          <SearchField onSearch={onSearch} onBlur={resetFilter} />
-        </span>
-        <CardList filteredRentals={filteredRentals} />
-      </div>
+            <SearchField onSearch={onSearch} onBlur={resetFilter} />
+          </span>
+          <CardList filteredRentals={filteredRentals} />
+        </div>
       </div>
     </div>
   );
